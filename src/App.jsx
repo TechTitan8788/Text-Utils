363e3570087dd066e942a02d390a9097a0a78f59
Tech Titans <lokhandeVishal8788@gmail.com>
2024-12-21 08:41:13 +0000

    add navbar & TextArea

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+
+function App() {
+  return (
+    <div>
+      <Navbar title="Text Utils" aboutText="About Us" />
+
+      <div className="container my-3">
+        <TextForm heading="Enter a Text To Analysed" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
